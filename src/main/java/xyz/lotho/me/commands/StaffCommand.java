64d451e4dedf9f1a66e37bb421d99cfc734c48c2
Lotho <58112436+MLotho@users.noreply.github.com>
2022-02-25 13:15:47 +0000

    added server ping method to disallow servers from displaying if they are offline, as well as showing max playercount in stafflist.

--- src/main/java/xyz/lotho/me/commands/StaffCommand.java
+++ src/main/java/xyz/lotho/me/commands/StaffCommand.java
@@ -1,7 +1,9 @@
 package xyz.lotho.me.commands;
 
 import net.luckperms.api.model.user.User;
+import net.md_5.bungee.api.Callback;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.ServerPing;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
@@ -10,6 +12,7 @@ import xyz.lotho.me.SkyStaff;
 import xyz.lotho.me.utils.Chat;
 
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 public class StaffCommand extends Command {
 
@@ -56,27 +59,35 @@ public class StaffCommand extends Command {
                     if (server == null) return;
                     if (server.getPlayers().stream().noneMatch((user) -> user.hasPermission(permission)) && !this.instance.config.getConfig().getBoolean("utils.showServersWithoutStaff")) return;
 
-                    Collection<ProxiedPlayer> players = server.getPlayers();
+                    server.ping((result, error) -> {
+                        if (error == null && result != null && result.getVersion() != null) {
+                            Collection<ProxiedPlayer> players = server.getPlayers();
 
-                    String serverLine = this.instance.config.getConfig().getString("messages.serverLine").replace("{server}", server.getName()).replace("{players}", String.valueOf(players.size()));
-                    builder.append("\n").append(Chat.colorize(serverLine));
+                            String serverLine = instance.config.getConfig().getString("messages.serverLine")
+                                    .replace("{server}", server.getName())
+                                    .replace("{players}", String.valueOf(players.size()))
+                                    .replace("{maxPlayers}", String.valueOf(result.getPlayers().getMax()));
 
-                    if (staff.size() == 0) {
-                        builder.append("\n").append(Chat.colorize(this.instance.config.getConfig().getString("messages.noStaffLine"))).append("\n");
-                    } else {
-                        staff.forEach((staffMember) -> {
-                            String staffLine = this.instance.config.getConfig().getString("messages.staffLine")
-                                    .replace("{prefix}", this.instance.staffManager.getStaffPrefix(staffMember))
-                                    .replace("{player}", staffMember.getDisplayName())
-                                    .replace("{time}", this.instance.staffManager.getLoginTimeFormatted(staffMember));
+                            builder.append("\n").append(Chat.colorize(serverLine));
 
-                            builder.append(Chat.colorize("\n" + staffLine));
-                        });
-                        builder.append("\n");
-                    }
+                            if (staff.size() == 0) {
+                                builder.append("\n").append(Chat.colorize(instance.config.getConfig().getString("messages.noStaffLine"))).append("\n");
+                            } else {
+                                staff.forEach((staffMember) -> {
+                                    String staffLine = instance.config.getConfig().getString("messages.staffLine")
+                                            .replace("{prefix}", instance.staffManager.getStaffPrefix(staffMember))
+                                            .replace("{player}", staffMember.getDisplayName())
+                                            .replace("{time}", instance.staffManager.getLoginTimeFormatted(staffMember));
+
+                                    builder.append(Chat.colorize("\n" + staffLine));
+                                });
+                                builder.append("\n");
+                            }
+                        }
+                        player.sendMessage(new TextComponent(builder.toString()));
+                    });
                 });
             }
-            player.sendMessage(new TextComponent(builder.toString()));
         }
     }
 };
